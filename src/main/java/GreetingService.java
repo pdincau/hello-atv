dd94956a501b5cda7ead543ace7ba064381d5ee3
Paolo D'Incau <paolo.dincau@gmail.com>
2017-11-22 10:25:33 +0100

    add tests

--- src/main/java/GreetingService.java
+++ src/main/java/GreetingService.java
@@ -1,6 +1,9 @@
 class GreetingService {
 
     public String greet(String name) {
+        if ("".equals(name)) {
+            return "Hello stranger! What's your name";
+        }
         return "Hello " + name + "!";
     }
 
--- src/test/java/GreetingServiceTest.java
+++ src/test/java/GreetingServiceTest.java
@@ -5,8 +5,21 @@ import static org.junit.Assert.assertEquals;
 public class GreetingServiceTest {
 
     @Test
-    public void it_pass() {
-        assertEquals("Hello Ivo!", "Hello Ivo!");
+    public void it_greets_people() {
+        GreetingService service = new GreetingService();
+
+        String message = service.greet("Ivo");
+
+        assertEquals("Hello Ivo!", message);
+    }
+
+    @Test
+    public void it_generates_an_error_message_when_name_is_empty() {
+        GreetingService service = new GreetingService();
+
+        String message = service.greet("");
+
+        assertEquals("Hello stranger! What's your name", message);
     }
 
 }
