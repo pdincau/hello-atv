44f34458109edfb7b1ec3cc2887c2832f81267d8
Paolo D'Incau <paolo.dincau@gmail.com>
2017-11-22 10:44:26 +0100

    add docker image build and push

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -1,6 +1,7 @@
 node {
 
     def mavenHome = tool 'Maven'
+    def app
 
     stage("Checkout and test") {
         checkout scm
@@ -9,5 +10,11 @@ node {
 
     stage("Build") {
         sh "${mavenHome}/bin/mvn package"
+        app = doker.build("pdincau/hello-ivo")
+    }
+
+    stage("Push image") {
+        app.push("${env.BUILD_NUMBER}")
+        app.push("latest")
     }
 }
