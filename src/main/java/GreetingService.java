2f5cc70985a8b086d72f4d7411cc69e8d49dbf0b
Paolo D'Incau <paolo.dincau@gmail.com>
2017-11-25 17:02:25 +0100

    fixes tests

--- src/main/java/GreetingService.java
+++ src/main/java/GreetingService.java
@@ -2,7 +2,7 @@ class GreetingService {
 
     public String greet(String name) {
         if ("".equals(name)) {
-            return "Hello Stranger";
+            return "Hello Stranger!";
         }
         return "Hello " + name + "!";
     }
